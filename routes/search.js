c689ce0e065bf612db6867d9c38d6d4d915e53c4
Vineet Ranjan <vineetranjan65@gmail.com>
2020-07-02 15:41:59 +0530

    Added Testing Centres to MongoDB

--- routes/search.js
+++ routes/search.js
@@ -17,7 +17,6 @@ router.get('/', (req,res) => {
 // Display Testing Centres Details
 router.post('/', (req,res) => {
     Testing.find({ state: req.body.state }, (err,results) => {
-        console.log(results);
         if (!err) {
             if (!results.length ) {
                 req.flash('error_msg', 'Error. Please refresh the page.');
@@ -38,19 +37,20 @@ router.post('/', (req,res) => {
 });
 
 // To add Testing Centres Details
-router.post('/add', (req,res) => {
-    const { state, hospitalName} = req.body;
-    const newTesting = new Testing({
-        state,
-        hospitalName
-    });
-
-    newTesting.save()
-        .then(testing => {
-            res.json({"success":testing});
-        })
-        .catch(err => console.log(err));
-})
+// Add promise
+// Input should be [{"state":"", "hospotalName":""},{...}]
+// router.post('/add', (req,res) => {
+//     data = req.body
+//     data.forEach(element => {
+//         const { state, hospitalName} = element;
+//         const newTesting = new Testing({
+//             state,
+//             hospitalName
+//         })  
+//         newTesting.save()
+//     })
+//     return res.json({"success":"OK"})
+// })
 
 
 module.exports = router;
